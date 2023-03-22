1a8c9ccf87c2805ceb4a17d7f64e2e390f15ddd3
Husam-H-Yahya <husamhisham220@gmail.com>
2023-03-22 17:48:28 +0200

    new update

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('about', function () {
+    $name = 'Husam';
+   // return view('about', ['name' => 'Husam']);
+   // return view('about')->with(['name', $name]);
+    return view('about',compact('name'));
+});
+
+Route::post('about', function () {
+    $name = 'Husam';
+    if(isset($_POST['name']))
+        $name = $_POST['name'];
+    return view('about',compact('name'));
+});
+
+Route::get('tasks', function () {
+    $tasks = ['Task #1', 'Task #2', 'Task #3'];
+    return view('tasks', compact('tasks'));
 });
